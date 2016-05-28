96e6f07848c0ce934796dc37e80d1a1b960e17e0
Raymond Cano <raymond@Raymonds-MacBook-Pro.local>
2016-05-28 11:48:12 -0400

    Initial commit of the trading project

---
+++ record_trades.py
@@ -0,0 +1,11 @@
+# Author: Raymond Cano
+# Script to record the trades from the past 30 so years of NBA Basketball. 
+# Maybe we find something interesting.
+
+def record_trades():
+	"""
+	Record the trades
+	"""
+
+	
+
